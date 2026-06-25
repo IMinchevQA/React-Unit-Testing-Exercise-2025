c45f6e7da9785a0e04745dd27de5829f1f45d000
IMinchevQA <i.minchev.qa@gmail.com>
2026-06-25 11:18:29 +0300

    Add basic tests for addition and division of numbers.

---
+++ src/utils/calcUtils.spec.js
@@ -0,0 +1,40 @@
+import { test, it, assert, expect } from 'vitest';
+import { add, divide } from './calcUtils';
+
+// test('Addition works correctly', () => {});
+
+it('Should return positive number when adding two positive numbers', () => {
+    //AAA Pattern
+
+    // Arrange
+    const firstNumber = 1;
+    const secondNumber = 2;
+    const expectedResult = 3;
+    
+    // Act
+    const actualResult = add(firstNumber, secondNumber);
+
+    // Assert
+
+    // TDD - test driven check
+    assert.equal(expectedResult, actualResult);
+
+    // BDD - behavior driven check
+    expect(actualResult).toBe(expectedResult);
+});
+
+it('Should return negative result number when adding two negative numbers', () => {
+    const firstNumber = -1;
+    const secondNumber = -2;
+    const expectedResult = -3;
+
+    expect(add(firstNumber, secondNumber)).toBe(expectedResult);
+});
+
+it('Should return half the number value after division by two', () => {
+    const inputNumber = 6;
+    const dividerNumber = 2;
+    const expectedResult = 3;
+
+    expect(divide(inputNumber, dividerNumber)).toBe(expectedResult);
+})
