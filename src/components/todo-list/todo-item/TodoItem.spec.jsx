48d5e11326b95451f5b9e562a5b93912e9eef1e3
IMinchevQA <i.minchev.qa@gmail.com>
2026-06-26 18:31:44 +0300

    unitTestMocking - use vi.fn() for mocking of a clickHandler.

--- src/components/todo-list/todo-item/TodoItem.spec.jsx
+++ src/components/todo-list/todo-item/TodoItem.spec.jsx
@@ -1,5 +1,5 @@
-import { render, screen } from '@testing-library/react';
-import { expect, it } from 'vitest';
+import { fireEvent, render, screen } from '@testing-library/react';
+import { expect, it, vi } from 'vitest';
 import TodoItem from './TodoItem';
 
 
@@ -20,10 +20,44 @@ it('Should have line-through when completed', () => {
 });
 
 it('Should NOT have line-through when completed', () => {
-    render(<TodoItem text="Test Todo" complete={false}/>);
+    render(<TodoItem text="Test Todo" complete={false} />);
 
     const listItem = screen.getByRole('listitem');
 
     expect(listItem).not.toHaveStyle('text-decoration: line-through');
     expect(listItem).toHaveStyle('text-decoration: none');
 });
+
+it('Should call onClick handler when todo item is clicked', () => {
+    const clickHandler = vi.fn();
+
+    render(<TodoItem text="Test Todo" completed={false} onClickHandler={clickHandler} />);
+
+    const listItem = screen.getByRole('listitem');
+
+    fireEvent.click(listItem);
+    expect(clickHandler).toHaveBeenCalled();
+    expect(clickHandler).toHaveBeenCalledOnce();
+    
+    expect(clickHandler).not.toHaveBeenCalledTimes(2);
+    fireEvent.click(listItem);
+    expect(clickHandler).toHaveBeenCalledTimes(2);
+});
+
+it('Should call onClick handler by providing particular id when todo item is clicked', () => {
+    const todoId = 1;
+    const clickHandler = vi.fn();
+
+    render(<TodoItem id={todoId} text="Test Todo" completed={false} onClickHandler={clickHandler} />);
+
+    const listItem = screen.getByRole('listitem');
+
+    fireEvent.click(listItem);
+    const desiredCallIndex = 0;
+    const desiredParamIndex = 0; 
+
+    expect(clickHandler.mock.calls[desiredCallIndex][desiredParamIndex]).toEqual(todoId);
+    expect(clickHandler).toHaveBeenCalledWith(todoId);
+
+
+})
